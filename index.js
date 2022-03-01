006ebba22c8f053220dac6fb0fde9c28a6bcc752
IonathanG <ionathangusmini@outlook.com>
2022-03-01 23:45:02 +0800

    v1_JS

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const navItem = document.querySelector('.nav');
 const hamburger = document.querySelector('.hamburger');
+const shortBtn = document.getElementById('shorten-it');
+let link;
 
 //opens and closes the mobile menu
 const mobile_Menu = () => {
@@ -8,7 +10,7 @@ const mobile_Menu = () => {
 }
 
 //closes the menu if click outside or it or on a menu item
-function closeMenu(e) {
+const closeMenu = (e) => {
     if (!(e.target.classList.contains('nav') || e.target.classList.contains('nav-menu')
         || e.target.classList.contains('hamburger')
         || e.target.classList.contains('bar'))) {
@@ -18,6 +20,26 @@ function closeMenu(e) {
         }
 }
 
+//function to manage the click for API shorten-it
+const shorten_It = (e) => {
+    e.preventDefault();
 
+    const linkInput = document.getElementById('linkInput');
+    console.log(linkInput.value);
+};
+
+
+class newLink {
+    constructor() {
+        this.longLink = document.getElementById('linkInput');
+    }
+
+}
+
+
+// inputName.addEventListener('input', (e) => {
+//     pseudo = e.target.value; //e.target.value important
+// });
 hamburger.addEventListener('click', mobile_Menu);
 window.addEventListener('click', (e) => closeMenu(e));
+shortBtn.addEventListener('click', (e) => shorten_It(e));
